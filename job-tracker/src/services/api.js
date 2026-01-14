980d5d28c8d36dfc31e8f8f3820291e458ebc2fd
ViniScooper <vviniciuslourenco@gmail.com>
2026-01-14 10:19:00 -0300

    deploy

--- job-tracker/src/services/api.js
+++ job-tracker/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://job-tracker-1-e7fg.onrender.com'
 });
 
 // Interceptor para adicionar o token em todas as requisições
